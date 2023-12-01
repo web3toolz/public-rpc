63c9497e49de6b165c281002af1e8541aace1c05
Nikita <nikitosnikn@yandex.ru>
2023-12-01 19:56:02 +0100

    Add Rpc model

---
+++ ui/src/models/rpc.ts
@@ -0,0 +1,11 @@
+export interface Rpc {
+    id: string;
+    http: string;
+    ws: string;
+    provider: string;
+    chain: string;
+    network: string;
+    status: string;
+    addedAt: string;
+    checkedAt: string;
+}
