2c2a0a9fb6e97803b6732c7cde7e96209306e623
Nikita <nikitosnikn@yandex.ru>
2023-12-09 19:44:21 +0100

    misc changes

--- backend/models/rpc.go
+++ backend/models/rpc.go
@@ -12,17 +12,24 @@ const (
 )
 
 type RPC struct {
-	Id       string `json:"id" bson:"_id"`
-	HTTP     string `json:"http" bson:"http"`
-	WS       string `json:"ws" bson:"ws"`
-	Provider string `json:"provider" bson:"provider"`
+	Id   string `json:"id" yaml:"id" bson:"_id"`
+	HTTP string `json:"http" yaml:"http" bson:"http"`
+	WS   string `json:"ws" yaml:"ws" bson:"ws"`
 
-	Status Status `json:"status" bson:"status"`
+	Status Status `json:"status" yaml:"Status" bson:"status"`
 
-	ChainId string `json:"chainId" bson:"chainId"`
-	Chain   string `json:"chain" bson:"chain"`
-	Network string `json:"network" bson:"network"`
+	ChainId  string `json:"chainId" yaml:"chainId" bson:"chainId"`
+	Chain    string `json:"chain" yaml:"chain" bson:"chain"`
+	Network  string `json:"network" yaml:"network" bson:"network"`
+	Provider string `json:"provider" yaml:"provider" bson:"provider"`
 
-	AddedAt   time.Time `json:"addedAt" bson:"addedAt"`
-	CheckedAt time.Time `json:"checkedAt" bson:"checkedAt"`
+	AddedAt   time.Time `json:"addedAt" yaml:"addedAt" bson:"addedAt"`
+	CheckedAt time.Time `json:"checkedAt" yaml:"checkedAt" bson:"checkedAt"`
+}
+
+func CompareRpcMetadata(rpc1, rpc2 *RPC) bool {
+	return rpc1.ChainId == rpc2.ChainId &&
+		rpc1.Chain == rpc2.Chain &&
+		rpc1.Network == rpc2.Network &&
+		rpc1.Provider == rpc2.Provider
 }
